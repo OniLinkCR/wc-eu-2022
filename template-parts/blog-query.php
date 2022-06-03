be5602151947ddab93231deb4ebe06e72b4c19b3
Marco Berrocal <hyrule@Marcos-MacBook-Pro.local>
2022-06-03 16:09:47 +0100

    Examples of  array for WP_Query class

--- template-parts/blog-query.php
+++ template-parts/blog-query.php
@@ -1,11 +1,93 @@
 <?php
 
     // Get 5 posts
+    // $wp_query_args = array(
+    //     'post_type'         => 'post',
+    //     'posts_per_page'    => '5',
+    // );
+
+    // Get 5 posts of type trips and food and oldest to newest
+    // $wp_query_args = array(
+    //     'post_type'         => array( 'trips','food'),
+    //     'posts_per_page'    => '5',
+    //     'order'             => 'ASC',
+    // );
+
+    // Get 10 posts and pages of author wc-eu-author
+    // $wp_query_args = array(
+    //     'post_type'         => array( 'page', 'post'),
+    //     'author_name'       => 'wc-eu-author',
+    //     'posts_per_page'    => '10',
+    // );
+
+    // Get all posts that have these two categories
+    // $wp_query_args = array(
+    //     'category_name' => 'uncategorized+news'
+    // );
+
+    // How to get popular posts. Two ways:
+    // $wp_query_args = array(
+    //     'post_type'         => 'post',
+    //     'posts_per_page'    => '1',
+    //     'orderby'           => 'comment_count'
+    // );
+
+    // $wp_query_args = array(
+    //     'post_type'         => 'post',
+    //     'comment_count' => array(
+    //         'value' => 1,
+    //         'compare' => '>=',
+    //     )
+    // );
+
+    // Get 1 posts of type trips or food that is in Europe or Latin America taxonomy
+    // $wp_query_args = array(
+    //     'post_type'         => array( 'trips', 'food'),
+    //     'posts_per_page'    => '1',
+    //     'orderby'           => 'rand',
+    //     'tax_query' => array(
+    //         //'relation' => 'AND',
+    //         array(
+    //             'taxonomy' => 'region',
+    //             'field'    => 'slug',
+    //             'terms'    => array( 'europe', 'latin-america' ),
+    //         )
+    //     )
+    // );
+
+
+    // $wp_query_args = array(
+    //     'post_type'         => array( 'post', 'trips', 'food'),
+    //     'posts_per_page'    => '2',
+    //     'orderby'           => 'rand',
+    //     'tax_query' => array(
+    //         'relation' => 'OR',
+    //         array(
+    //             'taxonomy' => 'region',
+    //             'field'    => 'slug',
+    //             'terms'    => array( 'europe', 'latin-america' ),
+    //         ),
+    //         array(
+    //             'taxonomy' => 'category',
+    //             'field'    => 'slug',
+    //             'terms'    => array( 'recipes', 'news' ),
+    //         )
+    //     )
+    // );
+
+    // Get 3 posts of type trips or food that is in Europe or Latin America taxonomy
     $wp_query_args = array(
-        'post_type'         => 'post',
-        'posts_per_page'    => '5',
+        'post_type'         => array( 'post', 'trips', 'food'),
+        'posts_per_page'    => '-1',
+        'orderby'           => 'rand',
+        'meta_key'         => '_title',
+        'meta_value'       => 'blue',
+        'no_found_rows' => true
     );
-    
+ 
+
+
+    echo the_title();
 
     $wceu_wp_query = new WP_Query($wp_query_args);
 
@@ -17,6 +99,9 @@
 
             <h2><?php the_title()?></h2>
             <?php the_content()?>
+            <?php the_author(); ?>
+            <?php $meta = get_post_meta(get_the_ID(), '_title', true );
+            echo $meta; ?>
         
         <?php endwhile;
 
